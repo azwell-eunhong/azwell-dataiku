6100ee236d5705180933ab9400977c7ef41012a1
eh-azwell <eunhong.choi@azwell.ai>
2025-07-15 15:44:37 +0900

    ..

--- dockerbuild/build/entrypoint.sh
+++ dockerbuild/build/entrypoint.sh
@@ -13,6 +13,22 @@ NODE_TYPE=${NODE_TYPE}
 
 start_dss() {
     echo "Starting DSS..."
+#    if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
+#            # Initialize new data directory
+#            ./dataiku-dss-${DSS_VERSION}/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
+#            ${DSS_HOME}/bin/dssadmin install-R-integration
+#            ${DSS_HOME}/bin/dssadmin install-graphics-export
+#            echo "dku.registration.channel=docker-image" >>${DSS_HOME}/config/dip.properties
+#            echo "dku.exports.chrome.sandbox=false" >>${DSS_HOME}/config/dip.properties
+#
+#    elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLDIR"') != "$DSS_INSTALLDIR" ]; then
+#            # Upgrade existing data directory
+#            rm -rf "$DSS_DATADIR"/pyenv
+#            ./dataiku-dss-${DSS_VERSION}/installer.sh -d "$DSS_DATADIR" -u -y
+#            ${DSS_HOME}/bin/dssadmin install-R-integration
+#            ${DSS_HOME}/bin/dssadmin install-graphics-export
+#
+#    fi
     ${DSS_HOME}/bin/dss start
     ${DSS_HOME}/bin/dss status
     tail -f ${DSS_HOME}/run/install.log
@@ -23,10 +39,10 @@ stop_dss() {
     ${DSS_HOME}/bin/dss stop
 }
 
-restart_dss() {
-    echo "Restarting DSS..."
-    ${DSS_HOME}/bin/dss restart
-    ${DSS_HOME}/bin/dss status
+restart_dss() { 
+    echo "Restarting DSS..." 
+     ${DSS_HOME}/bin/dss restart
+     ${DSS_HOME}/bin/dss status
 }
 
 health_check() {
@@ -55,8 +71,10 @@ install_dss() {
         echo "Installing DSS version: DSS_VERSION=${DSS_VERSION}" NODE_TYPE="${NODE_TYPE}"
     fi    
     ./dataiku-dss-${DSS_VERSION}/installer.sh -t ${NODE_TYPE} -d ${DSS_HOME} -p 11000
-    $DSS_HOME/bin/dssadmin install-R-integration
+    ${DSS_HOME}/bin/dssadmin install-R-integration
     ${DSS_HOME}/bin/dssadmin install-graphics-export
+
+    exit 0
 }
 upgrade_dss() {
     echo "Upgrading DSS..."
@@ -70,11 +88,13 @@ upgrade_dss() {
         NODE_TYPE="design"
         echo "DSS version not specified. Using default: ${NODE_TYPE}"
     else
-        echo "Installing DSS version: NODE_TYPE="${NODE_TYPE}"
+        echo "Installing DSS version:${DSS_VERSION} NODE_TYPE:${NODE_TYPE}"
     fi    
     ./dataiku-dss-${DSS_VERSION}/installer.sh -t ${NODE_TYPE} -d ${DSS_HOME} -u -y
-    $DSS_HOME/bin/dssadmin install-R-integration
+    ${DSS_HOME}/bin/dssadmin install-R-integration
     ${DSS_HOME}/bin/dssadmin install-graphics-export
+
+    exit 0
     
  }
 
